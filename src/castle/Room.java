e780883bfa7a98c1dee47870dfa48678f024efdd
liaotl10 <tianlinliao@163.com>
2017-07-08 12:56:50 +0800

    可扩展性：便于增加新的direction

--- src/castle/Room.java
+++ src/castle/Room.java
@@ -1,29 +1,20 @@
 package castle;
 
+import java.util.HashMap;
+
 public class Room {
-    public String description;
-    private Room northExit;
-    private Room southExit;
-    private Room eastExit;
-    private Room westExit;
+    private String description;
+    private HashMap<String, Room> exits=new HashMap<String,Room>();
 
     public Room(String description) 
     {
         this.description = description;
     }
 
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+    public void setExits(String dir,Room room) {
+    	exits.put(dir, room);
     }
-
+    
     @Override
     public String toString()
     {
@@ -32,35 +23,14 @@ public class Room {
     
     public String getExitDesc(){
     	StringBuffer buffer=new StringBuffer();
-    	if(northExit!=null){
-    		buffer.append("north ");
-    	}
-    	if(eastExit!=null){
-    		buffer.append("east ");
-    	}
-    	if(westExit!=null){
-    		buffer.append("west ");
-    	}
-    	if(southExit!=null){
-    		buffer.append("south ");
+    	for(String dir:exits.keySet()){
+    		buffer.append(dir);
+    		buffer.append(' ');
     	}
     	return buffer.toString();
     }
     
     public Room getExit(String direction){
-    	Room nextRoom=null;
-    	if(direction.equals("north")) {
-            nextRoom = northExit;
-        }
-        if(direction.equals("east")) {
-            nextRoom = eastExit;
-        }
-        if(direction.equals("south")) {
-            nextRoom = southExit;
-        }
-        if(direction.equals("west")) {
-            nextRoom = westExit;
-        }
-    	return nextRoom;
+    	return exits.get(direction);
     }
 }
